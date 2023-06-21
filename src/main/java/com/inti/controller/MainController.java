dd1fa6af9dc3fb3a692c6fea5b760e7a4d3e1c09
Gurvaan <Gurvan@DESKTOP-S8E7TRO>
2023-06-21 15:21:26 +0200

    Ajout d'un controller pour gérer la page d'acceuil + menu + page index renommée

---
+++ src/main/java/com/inti/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.inti.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	@GetMapping("/")
+	public String main() {
+		return "Index";
+	}
+}
