d84a8f73901e0a487103ca2bd9b2253c55f9288c
Daenrya <marjolaine.arb@gmail.com>
2023-06-21 13:27:43 +0200

    app properties

---
+++ src/main/java/com/inti/repository/IChefOrchestreRepository.java
@@ -0,0 +1,7 @@
+package com.inti.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IChefOrchestreRepository extends JpaRepository<ChefOrchestre, >{
+
+}
