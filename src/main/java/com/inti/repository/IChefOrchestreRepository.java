38a9a19573019383729e031c1d9d183dfc5f6819
Gurvaan <Gurvan@DESKTOP-S8E7TRO>
2023-06-21 14:49:54 +0200

    ajout des annotations pour repository

--- src/main/java/com/inti/repository/IChefOrchestreRepository.java
+++ src/main/java/com/inti/repository/IChefOrchestreRepository.java
@@ -1,9 +1,11 @@
 package com.inti.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.inti.model.ChefOrchestre;
 
+@Repository
 public interface IChefOrchestreRepository extends JpaRepository<ChefOrchestre, Integer>{
 
 }
