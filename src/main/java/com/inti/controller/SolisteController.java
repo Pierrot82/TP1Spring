d6ef2b99c6afa383962e756815d5591b99580c3a
Daenrya <marjolaine.arb@gmail.com>
2023-06-21 14:49:20 +0200

    Merge branch 'master' of https://github.com/Pierrot82/TP1Spring.git

---
+++ src/main/java/com/inti/controller/SolisteController.java
@@ -0,0 +1,62 @@
+package com.inti.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.inti.model.Soliste;
+import com.inti.repository.ISolisteRepository;
+
+@Controller
+public class SolisteController {
+	
+	@Autowired
+	ISolisteRepository isr;
+	
+	@GetMapping("creerSoliste")
+	public String formSoliste() {
+		return "formSoliste";
+	}
+
+	@PostMapping("saveSoliste")
+	public String saveSoliste(@ModelAttribute("soliste") Soliste s) {
+		isr.save(s);
+		return "redirect:/listeSoliste";
+	}
+
+	@GetMapping("listeSoliste")
+	public String listeSoliste(Model m) {
+		m.addAttribute("listeSoliste", isr.findAll().toArray());
+		System.out.println(isr.findAll());
+		return "listeSoliste";
+	}
+	@GetMapping("getSoliste")
+	public String getSoliste(@RequestParam("id") int id, Model m) {
+		m.addAttribute("soliste", isr.findById(id).get());
+		return "getSoliste";
+	}
+	@GetMapping("deleteSoliste")
+	public String deleteSoliste(@RequestParam("id") int id) {
+		isr.deleteById(id);
+		return "redirect:/listeSoliste";
+	}
+	
+	@GetMapping("modifierSoliste")
+	public String modifierSoliste(@RequestParam("id") int id, Model m)
+	{
+		m.addAttribute("soliste", isr.findById(id).get());
+		return "modifierSoliste";
+	}
+	
+	@PostMapping("updateSoliste")
+	public String updateSoliste(@ModelAttribute("soliste") Soliste s)
+	{
+		isr.save(s);
+		return "redirect:/listeSoliste";
+	}
+
+}
