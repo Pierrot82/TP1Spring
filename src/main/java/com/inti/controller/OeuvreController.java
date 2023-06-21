38530273d95a58e2cafb19ce26560cef801dc9fd
Pierrot82 <pierre.larroque.82@hotmail.fr>
2023-06-21 23:45:36 +0200

    Controller Creer/afficher/supp Oeuvre et formulaire,affichage associé

---
+++ src/main/java/com/inti/controller/OeuvreController.java
@@ -0,0 +1,61 @@
+package com.inti.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.inti.model.Oeuvre;
+import com.inti.repository.IOeuvreRepository;
+
+@Controller
+@RequestMapping("oeuvre")
+public class OeuvreController {
+	
+	@Autowired
+	IOeuvreRepository ior;
+	
+	@GetMapping("creerOeuvre")
+	public String formOeuvre() {
+		return "creerOeuvre";
+	}
+
+	@PostMapping("creerOeuvre")
+	public String saveOeuvre(@ModelAttribute("oeuvre") Oeuvre o) {
+		System.out.println(o);
+		ior.save(o);
+		return "redirect:/oeuvre/listeOeuvre";
+	}
+
+	@GetMapping("listeOeuvre")
+	public String listeOeuvre(Model m) {
+		m.addAttribute("listeOeuvre", ior.findAll().toArray());
+		return "listeOeuvre";
+	}
+
+	
+	@GetMapping("deleteOeuvre")
+	public String deleteOeuvre(@RequestParam("id") int id) {
+		ior.deleteById(id);
+		return "redirect:/listeOeuvre";
+	}
+	
+	@GetMapping("modifierOeuvre")
+	public String modifierOeuvre(@RequestParam("id") int id, Model m)
+	{
+		m.addAttribute("oeuvre", ior.findById(id).get());
+		return "modifierOeuvre";
+	}
+	
+	@PostMapping("modifierOeuvre")
+	public String updateOeuvre(@ModelAttribute("oeuvre") Oeuvre o)
+	{
+		ior.save(o); // = saveOrUpdate
+		return "redirect:/listeOeuvre";
+	}
+
+}
