ec36e40b8c9defa7d9974dc413653c04685cad26
Daenrya <marjolaine.arb@gmail.com>
2023-06-22 10:22:55 +0200

    crud chefO

--- src/main/java/com/inti/controller/ChefOrchestreController.java
+++ src/main/java/com/inti/controller/ChefOrchestreController.java
@@ -5,11 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.inti.model.ChefOrchestre;
+import com.inti.model.Soliste;
 import com.inti.repository.IChefOrchestreRepository;
 
 @Controller
@@ -27,7 +29,7 @@ public class ChefOrchestreController {
 	@PostMapping("saveChefOrchestre")
 	public String saveChefOrchestre(@ModelAttribute("chefOrchestre") ChefOrchestre co) {
 		icor.save(co);
-		return "redirect:/listeChefOrchestre";
+		return "redirect:/chef_orchestre/listeChefOrchestre";
 	}
 
 	@GetMapping("listeChefOrchestre")
@@ -35,24 +37,25 @@ public class ChefOrchestreController {
 		m.addAttribute("listeChefOrchestre", icor.findAll().toArray());
 		return "listeChefOrchestre";
 	}
-	@GetMapping("deleteChefOrchestre")
-	public String deleteChefOrchestre(@RequestParam("id") int id) {
+	@GetMapping("deleteChefOrchestre/{id}")
+	public String deleteChefOrchestre(@PathVariable("id") int id) {
 		icor.deleteById(id);
-		return "redirect:/listeChefOrchestre";
-	}
+		return "redirect:/chef_orchestre/listeChefOrchestre";
+
+	}	
 	
-	@GetMapping("modifierChefOrchestre")
-	public String modifierChefOrchestre(@RequestParam("id") int id, Model m)
+	@GetMapping("modifierChefOrchestre/{id}")
+	public String modifierChefOrchestre(@PathVariable("id") int id, Model m)
 	{
 		m.addAttribute("chefOrchestre", icor.findById(id).get());
 		return "modifierChefOrchestre";
 	}
 	
-	@PostMapping("updateChefOrchestre")
+	@PostMapping("modifierChefOrchestre/updateChefOrchestre")
 	public String updateChefOrchestre(@ModelAttribute("chefOrchestre") ChefOrchestre co)
 	{
 		icor.save(co);
-		return "redirect:/listeChefOrchestre";
+		return "redirect:/chef_orchestre/listeChefOrchestre";
 	}
-
+	
 }
