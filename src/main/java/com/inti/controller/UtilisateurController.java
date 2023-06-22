8832895ed9b8e1c4c4a1255e62f0525cb03aa876
Daenrya <marjolaine.arb@gmail.com>
2023-06-22 11:17:43 +0200

    Merge branch 'master' of https://github.com/Pierrot82/TP1Spring.git into HEAD
    save bdd utilisateur

--- src/main/java/com/inti/controller/UtilisateurController.java
+++ src/main/java/com/inti/controller/UtilisateurController.java
@@ -1,14 +1,31 @@
 package com.inti.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.inti.model.Soliste;
+import com.inti.model.Utilisateur;
+import com.inti.repository.IUtilisateurRepository;
 
 @Controller
 public class UtilisateurController {
 	
+	@Autowired
+	IUtilisateurRepository iur;
+	
 	@GetMapping("creerUtilisateur")
 	public String creerUtilisateur() {
 		return "creerUtilisateur";
+	}
+
+	@PostMapping("saveUtilisateur")
+	public String saveUtilisateur(@ModelAttribute("utilisateur") Utilisateur u) {
+		iur.save(u);
+		return "redirect:creerUtilisateur";
 	}
 
 }
